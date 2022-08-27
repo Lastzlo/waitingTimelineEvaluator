4768e0029115869872e88e0cb3129f66b05bb9d9
Lastzlo <z.last.zlo@gmail.com>
2022-08-27 18:31:48 +0300

    feat(QuestionType) add parseQuestionType

--- src/main/java/programmers/task/models/QuestionType.java
+++ src/main/java/programmers/task/models/QuestionType.java
@@ -12,14 +12,33 @@ public class QuestionType {
 		this.id = id;
 	}
 
-	public QuestionType(int id, Optional<Integer> categoryId) {
+	public QuestionType(int id, int categoryId) {
 		this.id = id;
-		this.categoryId = categoryId;
+		this.categoryId = Optional.of(categoryId);
 	}
 
-	public QuestionType(int id, Optional<Integer> categoryId, Optional<Integer> subCategoryId) {
+	public QuestionType(int id, int categoryId, int subCategoryId) {
 		this.id = id;
-		this.categoryId = categoryId;
-		this.subCategoryId = subCategoryId;
+		this.categoryId = Optional.of(categoryId);
+		this.subCategoryId = Optional.of(subCategoryId);
+	}
+
+	public static QuestionType parseQuestionType(String s) {
+		if (s.matches("[0-9]*\\.[0-9]*\\.[0-9]*")) {
+			String[] strings = s.split("\\.");
+			int id = Integer.parseInt(strings[0]);
+			int categoryId = Integer.parseInt(strings[1]);
+			int subCategoryId = Integer.parseInt(strings[2]);
+
+			return new QuestionType(id, categoryId, subCategoryId);
+		} else if (s.matches("[0-9]*\\.[0-9]*")) {
+			String[] strings = s.split("\\.");
+			int id = Integer.parseInt(strings[0]);
+			int categoryId = Integer.parseInt(strings[1]);
+
+			return new QuestionType(id, categoryId);
+		} else {
+			return new QuestionType(Integer.parseInt(s));
+		}
 	}
 }
---
+++ src/test/java/programmers/task/models/QuestionTypeTest.java
@@ -0,0 +1,15 @@
+package programmers.task.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionTypeTest {
+
+	@Test
+	void whenParseQuestionType() {
+		QuestionType.parseQuestionType("1334");
+		QuestionType.parseQuestionType("1234.1212");
+		QuestionType.parseQuestionType("1234.1212.3");
+	}
+}
