d72a09833b2800a97565a7802bd8bfba0f1975df
Lastzlo <z.last.zlo@gmail.com>
2022-08-27 18:11:14 +0300

    add Service to store service_id and variation_id

---
+++ src/main/java/programmers/task/models/Service.java
@@ -0,0 +1,47 @@
+package programmers.task.models;
+
+import java.util.Optional;
+
+class Service {
+	private final int id;
+	private Optional<Integer> variationId = Optional.empty();
+
+	public Service(int serviceId, int variationId) {
+		this.id = serviceId;
+		this.variationId = Optional.of(variationId);
+	}
+
+	public Service(int serviceId) {
+		this.id = serviceId;
+	}
+
+	public static Service parseService(String s) {
+		if (s.matches("[0-9]*\\.[0-9]")) {
+			String[] strings = s.split("\\.");
+			int serviceId = Integer.parseInt(strings[0]);
+			int variationId = Integer.parseInt(strings[1]);
+
+			return new Service(serviceId, variationId);
+		} else {
+			return new Service(Integer.parseInt(s));
+		}
+	}
+
+//	/**
+//	 * @param service the service we want to compare it to
+//	 * @return {@code true} if {@code this.id} and {@code this.variationId}
+//	 * the same or can wrap {@code service.id} and {@code service.variationId}
+//	 *
+//	 * For example, we have a query line with service_id.variation_id = 9.3,
+//	 * it can wrap the waiting timeline with service_id.variation_id = 9.3,
+//	 * or we have a query line with service_id.variation_id = 2,
+//	 * it can wrap the waiting timelines with service_id.variation_id = 2 and service_id.variation_id = 2.3 etc.
+//	 */
+//	public boolean canWrap(Service service) {
+//		if (this.id != service.id) return false;
+//		if (this.variationId.isPresent()) {
+//			if (service.variationId.isEmpty()) return false;
+//			return this.variationId.get().equals(service.variationId.get());
+//		} else return true;
+//	}
+}
---
+++ src/test/java/programmers/task/models/ServiceTest.java
@@ -0,0 +1,14 @@
+package programmers.task.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+	@Test
+	void whenParseService_thenCorrect() {
+		Service.parseService("12.3");
+		Service.parseService("2");
+	}
+}
