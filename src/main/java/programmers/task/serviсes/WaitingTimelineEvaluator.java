b6b2301a054dda14bc54e5baeedc2e7f0051285f
Lastzlo <z.last.zlo@gmail.com>
2022-08-27 10:10:44 +0300

    refactor(WaitingTimelineEvaluator.evaluate) add method handleLines, add throw IllegalArgumentException when Unknown line type

--- src/main/java/programmers/task/serviсes/WaitingTimelineEvaluator.java
+++ src/main/java/programmers/task/serviсes/WaitingTimelineEvaluator.java
@@ -7,41 +7,49 @@ import java.util.ArrayList;
 
 public class WaitingTimelineEvaluator {
 
-	public static ArrayList<String> evaluate (String input) {
+	public static ArrayList<String> evaluate(String input) {
 		String[] lines = input.split("\n");
 
 		int firstLineValue = Integer.parseInt(lines[0]);
-
 		int countOfAllLines = lines.length - 1;
-//		assert (firstLineValue != countOfAllLines);
 
-		if (firstLineValue != lines.length - 1) throw new IllegalArgumentException(
-				String.format("The value of the first line: \"%1$s\" does not correspond to the count of all lines: \"%2$s\"", firstLineValue, countOfAllLines));
+		if (firstLineValue == lines.length - 1) {
+			ArrayList<Query> queries = handleLines(lines);
 
-//		assert lines[lines.length - 1].startsWith("D");
+			ArrayList<String> result = new ArrayList<>();
+			for (int i = queries.size() - 1; i >= 0; i--) {
+				result.add(queries.get(i).getOutput());
+			}
+
+			return result;
+		} else {
+			throw new IllegalArgumentException(String.format(
+					"Issue with first line, the value of the first line: \"%1$s\" " +
+							"does not correspond to the count of all lines: \"%2$s\"",
+					firstLineValue,
+					countOfAllLines));
+		}
+
+	}
+
+	private static ArrayList<Query> handleLines(String[] lines) {
 		ArrayList<Query> queries = new ArrayList<>();
 		for (int i = lines.length - 1; i > 0; i--) {
 			String line = lines[i];
 
 			if (line.startsWith("D")) {
-				// add to queryLines
 				Query ql = Query.parseQuery(line);
 				queries.add(ql);
-			} else {
-				// map string to waitingTimelines
+			} else if (line.startsWith("C")) {
 				WaitingTimeLine wt = WaitingTimeLine.parseWaitingTimeLine(line);
 
-				// handle queries
 				queries.forEach(query -> query.handleWaitingTimeLine(wt));
+			} else {
+				throw new IllegalArgumentException(String.format(
+						"Unknown line type: \"%s\"", line
+				));
 			}
-
 		}
-
-		ArrayList<String> results = new ArrayList<>();
-		for (int i = queries.size() - 1; i >= 0; i--) {
-			results.add(queries.get(i).getOutput());
-		}
-
-		return results;
+		return queries;
 	}
 }
