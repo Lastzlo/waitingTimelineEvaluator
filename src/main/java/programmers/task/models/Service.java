8784728ce99aed7fd4e903c9196130515185416c
Lastzlo <z.last.zlo@gmail.com>
2022-08-28 11:41:17 +0300

    refactor(Service) add getters

--- src/main/java/programmers/task/models/Service.java
+++ src/main/java/programmers/task/models/Service.java
@@ -24,6 +24,18 @@ class Service {
 		return service;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public Optional<Integer> getVariationId() {
+		return variationId;
+	}
+
+	public boolean isMatchAll() {
+		return isMatchAll;
+	}
+
 	public static Service parseService(String s) {
 		if (s.matches("[0-9]*\\.[0-9]*")) {
 			String[] strings = s.split("\\.");
@@ -38,22 +50,22 @@ class Service {
 		}
 	}
 
-//	/**
-//	 * @param service the service we want to compare it to
-//	 * @return {@code true} if {@code this.id} and {@code this.variationId}
-//	 * the same or can wrap {@code service.id} and {@code service.variationId}
-//	 *
-//	 * For example, we have a query line with service_id.variation_id = 9.3,
-//	 * it can wrap the waiting timeline with service_id.variation_id = 9.3,
-//	 * or we have a query line with service_id.variation_id = 2,
-//	 * it can wrap the waiting timelines with service_id.variation_id = 2 and service_id.variation_id = 2.3 etc.
-//	 */
-//	public boolean canWrap(Service service) {
-//		if (isMatchAll) return true;
-//		if (this.id != service.id) return false;
-//		if (this.variationId.isPresent()) {
-//			if (service.variationId.isEmpty()) return false;
-//			return this.variationId.get().equals(service.variationId.get());
-//		} else return true;
-//	}
+	/**
+	 * @param service the service we want to compare it to
+	 * @return {@code true} if {@code this.id} and {@code this.variationId}
+	 * the same or can wrap {@code service.id} and {@code service.variationId}
+	 *
+	 * For example, we have a query line with service_id.variation_id = 9.3,
+	 * it can wrap the waiting timeline with service_id.variation_id = 9.3,
+	 * or we have a query line with service_id.variation_id = 2,
+	 * it can wrap the waiting timelines with service_id.variation_id = 2 and service_id.variation_id = 2.3 etc.
+	 */
+	public boolean canWrap(Service service) {
+		if (isMatchAll) return true;
+		if (this.id != service.id) return false;
+		if (this.variationId.isPresent()) {
+			if (service.variationId.isEmpty()) return false;
+			return this.variationId.get().equals(service.variationId.get());
+		} else return true;
+	}
 }
--- src/test/java/programmers/task/models/ServiceTest.java
+++ src/test/java/programmers/task/models/ServiceTest.java
@@ -8,8 +8,24 @@ class ServiceTest {
 
 	@Test
 	void whenParseService_thenCorrect() {
-		Service.parseService("12.3");
-		Service.parseService("2");
-		Service.parseService("*");
+		Service service = Service.parseService("12.3");
+
+		assertEquals(12, service.getId());
+		assertTrue(service.getVariationId().isPresent());
+		assertEquals(3, service.getVariationId().get());
+		assertFalse(service.isMatchAll());
+
+
+		service = Service.parseService("2");
+
+		assertEquals(2, service.getId());
+		assertFalse(service.getVariationId().isPresent());
+		assertFalse(service.isMatchAll());
+
+		service = Service.parseService("*");
+
+		assertEquals(0, service.getId());
+		assertFalse(service.getVariationId().isPresent());
+		assertTrue(service.isMatchAll());
 	}
 }
